7ae2100707844fc87269e7c1e475edf2e6e864b7
zy <zacwhy@gmail.com>
2021-09-24 20:21:21 +0800

    Add documentation

--- app/characterService.js
+++ app/characterService.js
@@ -39,6 +39,42 @@ export default class CharacterService {
     return value
   }
 
+  /**
+   * Syncing with remote Marvel API dataset
+   *
+   * Assumptions are that:
+   *   - when there is new Marvel character added, the modified date will be
+   *     set to that date
+   *   - when there is an update to the character's attributes, the modified
+   *     date will be updated to the new date
+   *
+   *
+   * When there is no character data cached localled, the sync process will
+   * begin by querying the remote Marvel API by using:
+   *
+   *   orderBy=modified,name & limit=100 & offset=0
+   *
+   * From the first response, we inspect the `data.count`. If the count is 100,
+   * we will assume that there are more data to be fetched, and continue
+   * fetching with a new offset:
+   *
+   *   orderBy=modified,name & limit=100 & offset=100
+   *
+   * This process will continue until `data.count` is less than 100. Then we
+   * know there should be no more data left to fetch.
+   *
+   *
+   * When we already have some cached data, the sync process will query the
+   * remote Marvel API with the `modifiedSince` filter.
+   *
+   * We first get the maximum modified date from the cache. Then query the
+   * Marvel API with:
+   *
+   *   modifiedSince = (maxModified as YYYY-MM-DD) - (1 day)
+   *
+   * We use one day before to make sure we do not miss out updates that was
+   * made later in the same day as the maxModified date.
+   */
   async syncWithRemote() {
     const maxModified = this.characterRepository.maxModified()
     console.log('maxModified', maxModified)
